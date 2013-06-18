75ce7dda828f21298892a7dc432ce89bfb3913b3
Eric Bunker <ericbunker@gmail.com>
2013-06-18 08:11:19 +0200

    final

--- sortiment.php
+++ sortiment.php
@@ -9,7 +9,13 @@ echo'
 $sql = new mysql();
 $sql->login("Simon","qwertz");
 $sql->winelist();
-echo "<a href='./common/excel.php' target='_self'><img src='./images/excel.png' width='80' height='80' border='0' alt='excel'></a>";
+echo "
+<div id='excel'>
+<h3>Weinliste als Excel Tabelle</h3>
+<p>
+<a href='./common/excel.php' target='_self'><img src='./images/excel.png' width='80' height='80' border='0' alt='excel'></a>
+</p>
+</div>";
 $sql->close_connect();
 		  
 include "./common/navigationFooter.php";
